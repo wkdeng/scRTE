f274c8755c5f59045edd1d23965c5f72bcebc796
Deng <wdeng3@sbcphaplp001.uthouston.edu>
2023-03-31 16:17:52 -0500

    1

--- scripts/TE_net.py
+++ scripts/TE_net.py
@@ -5,29 +5,29 @@
  # @modify date 2023-03-21 14:16:13
  # @desc [description]
 ###
-# from pybedtools import BedTool
-# import sys
-# import pandas as pd
-# # sys.argv=['test.opy','../../universal_data/rmsk/rmsk_GRCh38.txt','../../universal_data/ref/GRCh38/genes.bed']
-# rmsk=sys.argv[1]
-# gene_anno=sys.argv[2]
-# out=sys.argv[3]
+from pybedtools import BedTool
+import sys
+import pandas as pd
+# sys.argv=['test.opy','../../universal_data/rmsk/rmsk_GRCh38.txt','../../universal_data/ref/GRCh38/genes.bed']
+rmsk=sys.argv[1]
+gene_anno=sys.argv[2]
+out=sys.argv[3]
 
-# rmsk=sys.argv[1]
-# gene_anno=sys.argv[2]
+rmsk=sys.argv[1]
+gene_anno=sys.argv[2]
 
-# rmsk_df=pd.read_csv(rmsk,sep='\t')
-# rmsk_df['index']=rmsk_df[['repClass','repFamily','repName']].agg(':'.join,axis=1)
+rmsk_df=pd.read_csv(rmsk,sep='\t')
+rmsk_df['index']=rmsk_df[['repClass','repFamily','repName']].agg(':'.join,axis=1)
 
-# rmsk_bed=BedTool.from_dataframe(rmsk_df[['genoName','genoStart','genoEnd','index','swScore','strand']])
-# conn=rmsk_bed.intersect(anno_bed,s=True,wa=True,wb=True).to_dataframe()
-# conn[['ensg','gn']]=[x.split(':') for x in conn['blockCount']]
+rmsk_bed=BedTool.from_dataframe(rmsk_df[['genoName','genoStart','genoEnd','index','swScore','strand']])
+conn=rmsk_bed.intersect(anno_bed,s=True,wa=True,wb=True).to_dataframe()
+conn[['ensg','gn']]=[x.split(':') for x in conn['blockCount']]
 
-# print(len(conn))
-# conn['class']=[x.split(':')[0] for x in conn['name']]
-# conn=conn.loc[conn['class'].isin(['SINE','LTR','LINE'])]
-# conn=conn[['name','gn']].drop_duplicates()
-# conn[['name','gn']].to_csv('network.txt',header=True,index=False)
+print(len(conn))
+conn['class']=[x.split(':')[0] for x in conn['name']]
+conn=conn.loc[conn['class'].isin(['SINE','LTR','LINE'])]
+conn=conn[['name','gn']].drop_duplicates()
+conn[['name','gn']].to_csv('network.txt',header=True,index=False)
 
 import pandas as pd
 from collections import defaultdict
